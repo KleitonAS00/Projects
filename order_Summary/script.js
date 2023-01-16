4e3062e220ff26f77d8d1d6788bb845ac4c9f0ab
KleitonAS00 <kleitonsilva.24@hotmail.com>
2023-01-16 19:45:51 -0300

    função para alterar tela de planos

--- order_Summary/script.js
+++ order_Summary/script.js
@@ -1,1 +1,11 @@
-const 
+const change = document.querySelector('.change');
+const principalContent = document.getElementById('content');
+const secondaryContent = document.querySelector('#other-plans');
+
+
+change.addEventListener('click', function changePlan() {
+    principalContent.setAttribute('class', 'hidden')
+    if(principalContent.classList.contains('hidden')) {
+        principalContent.style.display = "none"
+    }
+})
