0dfa783db1e27c15004a8ba27a5d9219743140c7
KleitonAS00 <kleitonsilva.24@hotmail.com>
2023-01-18 19:06:48 -0300

    ajustes da segunda tela

--- order_Summary/script.js
+++ order_Summary/script.js
@@ -1,11 +1,12 @@
 const change = document.querySelector('.change');
 const principalContent = document.getElementById('content');
 const secondaryContent = document.querySelector('#other-plans');
+const cancelOrder = document.getElementById('cancel-order');
 
 
 change.addEventListener('click', function changePlan() {
-    principalContent.setAttribute('class', 'hidden')
+    principalContent.setAttribute('class', 'hidden');
     if(principalContent.classList.contains('hidden')) {
-        principalContent.style.display = "none"
+        principalContent.style.display = "none";
     }
 })
